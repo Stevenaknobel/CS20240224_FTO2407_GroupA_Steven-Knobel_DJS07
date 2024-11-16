8f5992b191b4678d20d262d4598acc032732b6ca
Steven <stevenaknobel@gmail.com>
2024-11-16 11:46:44 +0200

    update the code for the meme component and link it to memesdata to pull a random meme

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,33 +1,36 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        console.log(url)
+    }
+
     return (
         <main>
-            <form className="form">
-                <div>
-                    <label>Top Text
-                        <input
-                            type="text"
-                            placeholder="Shut up"
-                            className="form--input"
-                        />
-                    </label>
-                </div>
-                <div>
-                    <label>Bottom Text
-                    <input
-                            type="text"
-                            placeholder="and take my money"
-                            className="form--input"
-                        />
-                    </label>
-                </div>
-                <button
+            <div className="form">
+                <p>{url}</p>
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
